aosp-mirror/kernel_msm

ANDROID: cpufreq: Remove seq_printf from critical path
Calls to uid_time_in_state, uid_concurrent_*_time are
on the critical path when the screen changes. seq_printf
is taking a significant portion of the time. Replace
seq_printf with the faster seq_write function.

Since the files will not be human readable, move them
to a seperate directory. Leave a human readable version
of uid_time_in_state where it is for debugging and
testing purposes.

b/71718257

Change-Id: I599f7b67e8bcb5a584b0c88d759b2b82af2c2144
Signed-off-by: Marissa Wall <marissaw@google.com>

drivers/cpufreq/cpufreq_stats.c
@@ -63,6 +63,7 @@ static int cpufreq_max_state;
 static int cpufreq_last_max_state;
 static unsigned int *cpufreq_states;
 static bool cpufreq_stats_initialized;
+static struct proc_dir_entry *uid_cpupower;
 
 struct cpufreq_stats_attribute {
 	struct attribute attr;
@@ -132,6 +133,29 @@ static int account_thread_stats(struct task_struct *task,
 	return 0;
 }
 
+/* Caller must hold uid_lock */
+static int account_threads_stats(void)
+{
+	struct uid_entry *uid_entry;
+	struct task_struct *task, *temp;
+
+	rcu_read_lock();
+	do_each_thread(temp, task) {
+
+		uid_entry = find_or_register_uid(from_kuid_munged(
+			current_user_ns(), task_uid(task)));
+		if (!uid_entry)
+			continue;
+
+		account_thread_stats(
+			task,
+			&(uid_entry->alive_time_in_state),
+			&(uid_entry->alive_max_state));
+	} while_each_thread(temp, task);
+	rcu_read_unlock();
+	return 0;
+}
+
 static int single_uid_time_in_state_show(struct seq_file *m, void  *ptr)
 {
 	struct uid_entry *uid_entry;
@@ -192,11 +216,9 @@ static int single_uid_time_in_state_show(struct seq_file *m, void  *ptr)
 	return 0;
 }
 
-
 static int uid_time_in_state_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
-	struct task_struct *task, *temp;
 	unsigned long bkt;
 	struct cpufreq_policy *last_policy = NULL;
 	int i;
@@ -226,20 +248,7 @@ static int uid_time_in_state_show(struct seq_file *m, void *v)
 
 	rt_mutex_lock(&uid_lock);
 
-	rcu_read_lock();
-	do_each_thread(temp, task) {
-
-		uid_entry = find_or_register_uid(from_kuid_munged(
-			current_user_ns(), task_uid(task)));
-		if (!uid_entry)
-			continue;
-
-		account_thread_stats(
-			task,
-			&(uid_entry->alive_time_in_state),
-			&(uid_entry->alive_max_state));
-	} while_each_thread(temp, task);
-	rcu_read_unlock();
+	account_threads_stats();
 
 	hash_for_each(uid_hash_table, bkt, uid_entry, hash) {
 		int max_state = uid_entry->dead_max_state;
@@ -276,17 +285,87 @@ static int uid_time_in_state_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
+/*
+ * time_in_state is an array of u64's in the following format:
+ * [n, uid0, time0a, time0b, ..., time0n,
+ *     uid1, time1a, time1b, ..., time1n,
+ *     uid2, time2a, time2b, ..., time2n, etc.]
+ * where n is the total number of frequencies
+ */
+static int time_in_state_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
-	struct task_struct *task, *temp;
-	unsigned long bkt, flags;
-	int i, num_possible_cpus = num_possible_cpus();
+	unsigned long bkt;
+	u64 cpufreq_max_state_u64 = cpufreq_max_state;
+	u64 uid, time;
+	int i;
 
 	if (!cpufreq_stats_initialized)
 		return 0;
 
-	seq_printf(m, "active: %i\n", num_possible_cpus);
+	seq_write(m, &cpufreq_max_state_u64, sizeof(cpufreq_max_state_u64));
+
+	rt_mutex_lock(&uid_lock);
+
+	account_threads_stats();
+
+	hash_for_each(uid_hash_table, bkt, uid_entry, hash) {
+		int max_state = uid_entry->dead_max_state;
+
+		if (uid_entry->alive_max_state > max_state)
+			max_state = uid_entry->alive_max_state;
+
+		if (max_state) {
+			uid = (u64) uid_entry->uid;
+			seq_write(m, &uid, sizeof(uid));
+		}
+
+		for (i = 0; i < max_state; ++i) {
+			u64 total_time_in_state = 0;
+
+			if (uid_entry->dead_time_in_state &&
+				i < uid_entry->dead_max_state) {
+				total_time_in_state =
+					uid_entry->dead_time_in_state[i];
+			}
+			if (uid_entry->alive_time_in_state &&
+				i < uid_entry->alive_max_state) {
+				total_time_in_state +=
+					uid_entry->alive_time_in_state[i];
+			}
+			time = (u64)
+				cputime_to_clock_t(total_time_in_state);
+			seq_write(m, &time, sizeof(time));
+		}
+
+		kfree(uid_entry->alive_time_in_state);
+		uid_entry->alive_time_in_state = NULL;
+		uid_entry->alive_max_state = 0;
+	}
+
+	rt_mutex_unlock(&uid_lock);
+	return 0;
+}
+
+/*
+ * concurrent_active_time is an array of u64's in the following format:
+ * [n, uid0, time0a, time0b, ..., time0n,
+ *     uid1, time1a, time1b, ..., time1n,
+ *     uid2, time2a, time2b, ..., time2n, etc.]
+ * where n is the total number of cpus (num_possible_cpus)
+ */
+static int concurrent_active_time_show(struct seq_file *m, void *v)
+{
+	struct uid_entry *uid_entry;
+	struct task_struct *task, *temp;
+	u64 uid, time, num_possible_cpus = num_possible_cpus();
+	unsigned long bkt, flags;
+	int i;
+
+	if (!cpufreq_stats_initialized)
+		return 0;
+
+	seq_write(m, &num_possible_cpus, sizeof(num_possible_cpus));
 
 	rt_mutex_lock(&uid_lock);
 
@@ -323,7 +402,8 @@ static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
 			!uid_entry->dead_concurrent_active_time)
 			continue;
 
-		seq_printf(m, "%d:", uid_entry->uid);
+		uid = (u64) uid_entry->uid;
+		seq_write(m, &uid, sizeof(uid));
 
 		for (i = 0; i < num_possible_cpus; ++i) {
 			u64 total_concurrent_active_time = 0;
@@ -334,12 +414,10 @@ static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
 			if (uid_entry->alive_concurrent_active_time)
 				total_concurrent_active_time +=
 					uid_entry->alive_concurrent_active_time[i];
-			seq_printf(m, " %lu", (unsigned long)
-				cputime_to_clock_t(total_concurrent_active_time));
+			time = (u64)
+				cputime_to_clock_t(total_concurrent_active_time);
+			seq_write(m, &time, sizeof(time));
 		}
-
-		seq_putc(m, '\n');
-
 
 		if (uid_entry->alive_concurrent_active_time) {
 			kfree(uid_entry->alive_concurrent_active_time);
@@ -351,47 +429,48 @@ static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static int uid_concurrent_policy_time_show(struct seq_file *m, void *v)
+/*
+ * concurrent_policy_time is an array of u64's in the following format:
+ * [n, x0, ..., xn, uid0, time0a, time0b, ..., time0n,
+ *                  uid1, time1a, time1b, ..., time1n,
+ *                  uid2, time2a, time2b, ..., time2n, etc.]
+ * where n is the number of policies
+ * xi is the number cpus on a particular policy
+ */
+static int concurrent_policy_time_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
 	struct task_struct *task, *temp;
 	struct cpufreq_policy *policy;
 	struct cpufreq_policy *last_policy = NULL;
+	u64 buf[num_possible_cpus()];
+	u64 uid, time;
 	unsigned long bkt, flags;
-	int i, cnt = -1, num_possible_cpus = num_possible_cpus();
+	int i, cnt = 0, num_possible_cpus = num_possible_cpus();
 
 	if (!cpufreq_stats_initialized)
 		return 0;
 
 	for_each_possible_cpu(i) {
 		policy = cpufreq_cpu_get(i);
-		if (!policy) {
-			seq_printf(m, " -1");
+		if (!policy)
 			continue;
-		}
-
 		if (policy != last_policy) {
-			if (cnt >= 0)
-				seq_printf(m, "%d ", cnt);
-			cnt = 0;
-			seq_printf(m, "policy%d: ",
-				cpumask_first(policy->related_cpus));
-
+			cnt++;
 			if (last_policy)
 				cpufreq_cpu_put(last_policy);
 			last_policy = policy;
+			buf[cnt] = 0;
 		} else {
 			cpufreq_cpu_put(policy);
 		}
-		++cnt;
+		++buf[cnt];
 	}
 	if (last_policy)
 		cpufreq_cpu_put(last_policy);
 
-	if (cnt >= 0)
-		seq_printf(m, "%d ", cnt);
-
-	seq_putc(m, '\n');
+	buf[0] = (u64) cnt;
+	seq_write(m, buf, (cnt + 1) * sizeof(*buf));
 
 	rt_mutex_lock(&uid_lock);
 
@@ -428,7 +507,8 @@ static int uid_concurrent_policy_time_show(struct seq_file *m, void *v)
 			!uid_entry->dead_concurrent_policy_time)
 			continue;
 
-		seq_printf(m, "%d:", uid_entry->uid);
+		uid = (u64) uid_entry->uid;
+		seq_write(m, &uid, sizeof(uid));
 
 		for (i = 0; i < num_possible_cpus; ++i) {
 			u64 total_concurrent_policy_time = 0;
@@ -439,11 +519,10 @@ static int uid_concurrent_policy_time_show(struct seq_file *m, void *v)
 			if (uid_entry->alive_concurrent_policy_time)
 				total_concurrent_policy_time +=
 					uid_entry->alive_concurrent_policy_time[i];
-			seq_printf(m, " %lu", (unsigned long)
-				cputime_to_clock_t(total_concurrent_policy_time));
+			time = (u64)
+				cputime_to_clock_t(total_concurrent_policy_time);
+			seq_write(m, &time, sizeof(time));
 		}
-
-		seq_putc(m, '\n');
 
 		if (uid_entry->alive_concurrent_policy_time) {
 			kfree(uid_entry->alive_concurrent_policy_time);
@@ -1075,25 +1154,37 @@ static const struct file_operations uid_time_in_state_fops = {
 	.release	= single_release,
 };
 
-static int uid_concurrent_active_time_open(struct inode *inode, struct file *file)
+int time_in_state_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, uid_concurrent_active_time_show, PDE_DATA(inode));
+	return single_open(file, time_in_state_show, PDE_DATA(inode));
 }
 
-static const struct file_operations uid_concurrent_active_time_fops = {
-	.open		= uid_concurrent_active_time_open,
+const struct file_operations time_in_state_fops = {
+	.open		= time_in_state_open,
 	.read		= seq_read,
 	.llseek		= seq_lseek,
 	.release	= single_release,
 };
 
-static int uid_concurrent_policy_time_open(struct inode *inode, struct file *file)
+static int concurrent_active_time_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, uid_concurrent_policy_time_show, PDE_DATA(inode));
+	return single_open(file, concurrent_active_time_show, PDE_DATA(inode));
 }
 
-static const struct file_operations uid_concurrent_policy_time_fops = {
-	.open		= uid_concurrent_policy_time_open,
+static const struct file_operations concurrent_active_time_fops = {
+	.open		= concurrent_active_time_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int concurrent_policy_time_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, concurrent_policy_time_show, PDE_DATA(inode));
+}
+
+static const struct file_operations concurrent_policy_time_fops = {
+	.open		= concurrent_policy_time_open,
 	.read		= seq_read,
 	.llseek		= seq_lseek,
 	.release	= single_release,
@@ -1162,17 +1253,26 @@ static int __init cpufreq_stats_init(void)
 	proc_create_data("uid_time_in_state", 0444, NULL,
 		&uid_time_in_state_fops, NULL);
 
-	proc_create_data("uid_concurrent_active_time", 0444, NULL,
-		&uid_concurrent_active_time_fops, NULL);
-
-	proc_create_data("uid_concurrent_policy_time", 0444, NULL,
-		&uid_concurrent_policy_time_fops, NULL);
-
 	profile_event_register(PROFILE_TASK_EXIT, &process_notifier_block);
 
 	ret = cpufreq_stats_create_all_table();
 	if (ret)
 		pr_warn("Cannot create cpufreq all freqs table\n");
+
+	uid_cpupower = proc_mkdir("uid_cpupower", NULL);
+	if (!uid_cpupower) {
+		pr_warn("%s: failed to create uid_cputime proc entry\n",
+			__func__);
+	} else {
+		proc_create_data("time_in_state", 0444, uid_cpupower,
+			&time_in_state_fops, NULL);
+
+		proc_create_data("concurrent_active_time", 0444, uid_cpupower,
+			&concurrent_active_time_fops, NULL);
+
+		proc_create_data("concurrent_policy_time", 0444, uid_cpupower,
+			&concurrent_policy_time_fops, NULL);
+	}
 
 	cpufreq_stats_initialized = true;
 	return 0;

bullyliit07/kernel_xiaomi_vince

Change-Id: Ie947dd0caa26682a655a164b195ad72fd6693c1b
Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>
Signed-off-by: Oveno <ovenoboyo@gmail.com>

drivers/cpufreq/cpufreq_stats.c
@@ -83,6 +83,7 @@ struct all_freq_table {
 
 static struct all_freq_table *all_freq_table;
 static bool cpufreq_all_freq_init;
+static struct proc_dir_entry *uid_cpupower;
 
 static DEFINE_PER_CPU(struct all_cpufreq_stats *, all_cpufreq_stats);
 static DEFINE_PER_CPU(struct cpufreq_stats *, cpufreq_stats_table);
@@ -155,6 +156,29 @@ static int account_thread_stats(struct task_struct *task,
 	return 0;
 }
 
+/* Caller must hold uid_lock */
+static int account_threads_stats(void)
+{
+	struct uid_entry *uid_entry;
+	struct task_struct *task, *temp;
+
+	rcu_read_lock();
+	do_each_thread(temp, task) {
+
+		uid_entry = find_or_register_uid(from_kuid_munged(
+			current_user_ns(), task_uid(task)));
+		if (!uid_entry)
+			continue;
+
+		account_thread_stats(
+			task,
+			&(uid_entry->alive_time_in_state),
+			&(uid_entry->alive_max_states));
+	} while_each_thread(temp, task);
+	rcu_read_unlock();
+	return 0;
+}
+
 static int single_uid_time_in_state_show(struct seq_file *m, void  *ptr)
 {
 	struct uid_entry *uid_entry;
@@ -215,11 +239,9 @@ static int single_uid_time_in_state_show(struct seq_file *m, void  *ptr)
 	return 0;
 }
 
-
 static int uid_time_in_state_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
-	struct task_struct *task, *temp;
 	unsigned long bkt;
 	int i;
 
@@ -233,20 +255,7 @@ static int uid_time_in_state_show(struct seq_file *m, void *v)
 
 	rt_mutex_lock(&uid_lock);
 
-	rcu_read_lock();
-	do_each_thread(temp, task) {
-
-		uid_entry = find_or_register_uid(from_kuid_munged(
-			current_user_ns(), task_uid(task)));
-		if (!uid_entry)
-			continue;
-
-		account_thread_stats(
-			task,
-			&(uid_entry->alive_time_in_state),
-			&(uid_entry->alive_max_states));
-	} while_each_thread(temp, task);
-	rcu_read_unlock();
+	account_threads_stats();
 
 	hash_for_each(uid_hash_table, bkt, uid_entry, hash) {
 		int max_states = uid_entry->dead_max_states;
@@ -283,17 +292,87 @@ static int uid_time_in_state_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
+/*
+ * time_in_state is an array of u64's in the following format:
+ * [n, uid0, time0a, time0b, ..., time0n,
+ *     uid1, time1a, time1b, ..., time1n,
+ *     uid2, time2a, time2b, ..., time2n, etc.]
+ * where n is the total number of frequencies
+ */
+static int time_in_state_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
-	struct task_struct *task, *temp;
-	unsigned long bkt, flags;
-	int i, num_possible_cpus = num_possible_cpus();
+	unsigned long bkt;
+	u64 cpufreq_max_states = all_freq_table->table_size;
+	u64 uid, time;
+	int i;
 
 	if (!cpufreq_all_freq_init)
 		return 0;
 
-	seq_printf(m, "active: %i\n", num_possible_cpus);
+	seq_write(m, &cpufreq_max_states, sizeof(cpufreq_max_states));
+
+	rt_mutex_lock(&uid_lock);
+
+	account_threads_stats();
+
+	hash_for_each(uid_hash_table, bkt, uid_entry, hash) {
+		int max_state = uid_entry->dead_max_states;
+
+		if (uid_entry->alive_max_states > max_state)
+			max_state = uid_entry->alive_max_states;
+
+		if (max_state) {
+			uid = (u64) uid_entry->uid;
+			seq_write(m, &uid, sizeof(uid));
+		}
+
+		for (i = 0; i < max_state; ++i) {
+			u64 total_time_in_state = 0;
+
+			if (uid_entry->dead_time_in_state &&
+				i < uid_entry->dead_max_states) {
+				total_time_in_state =
+					uid_entry->dead_time_in_state[i];
+			}
+			if (uid_entry->alive_time_in_state &&
+				i < uid_entry->alive_max_states) {
+				total_time_in_state +=
+					uid_entry->alive_time_in_state[i];
+			}
+			time = (u64)
+				cputime_to_clock_t(total_time_in_state);
+			seq_write(m, &time, sizeof(time));
+		}
+
+		kfree(uid_entry->alive_time_in_state);
+		uid_entry->alive_time_in_state = NULL;
+		uid_entry->alive_max_states = 0;
+	}
+
+	rt_mutex_unlock(&uid_lock);
+	return 0;
+}
+
+/*
+ * concurrent_active_time is an array of u64's in the following format:
+ * [n, uid0, time0a, time0b, ..., time0n,
+ *     uid1, time1a, time1b, ..., time1n,
+ *     uid2, time2a, time2b, ..., time2n, etc.]
+ * where n is the total number of cpus (num_possible_cpus)
+ */
+static int concurrent_active_time_show(struct seq_file *m, void *v)
+{
+	struct uid_entry *uid_entry;
+	struct task_struct *task, *temp;
+	u64 uid, time, num_possible_cpus = num_possible_cpus();
+	unsigned long bkt, flags;
+	int i;
+
+	if (!cpufreq_all_freq_init)
+		return 0;
+
+	seq_write(m, &num_possible_cpus, sizeof(num_possible_cpus));
 
 	rt_mutex_lock(&uid_lock);
 
@@ -330,7 +409,8 @@ static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
 			!uid_entry->dead_concurrent_active_time)
 			continue;
 
-		seq_printf(m, "%d:", uid_entry->uid);
+		uid = (u64) uid_entry->uid;
+		seq_write(m, &uid, sizeof(uid));
 
 		for (i = 0; i < num_possible_cpus; ++i) {
 			u64 total_concurrent_active_time = 0;
@@ -341,12 +421,10 @@ static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
 			if (uid_entry->alive_concurrent_active_time)
 				total_concurrent_active_time +=
 					uid_entry->alive_concurrent_active_time[i];
-			seq_printf(m, " %lu", (unsigned long)
-				cputime_to_clock_t(total_concurrent_active_time));
+			time = (u64)
+				cputime_to_clock_t(total_concurrent_active_time);
+			seq_write(m, &time, sizeof(time));
 		}
-
-		seq_putc(m, '\n');
-
 
 		if (uid_entry->alive_concurrent_active_time) {
 			kfree(uid_entry->alive_concurrent_active_time);
@@ -358,47 +436,48 @@ static int uid_concurrent_active_time_show(struct seq_file *m, void *v)
 	return 0;
 }
 
-static int uid_concurrent_policy_time_show(struct seq_file *m, void *v)
+/*
+ * concurrent_policy_time is an array of u64's in the following format:
+ * [n, x0, ..., xn, uid0, time0a, time0b, ..., time0n,
+ *                  uid1, time1a, time1b, ..., time1n,
+ *                  uid2, time2a, time2b, ..., time2n, etc.]
+ * where n is the number of policies
+ * xi is the number cpus on a particular policy
+ */
+static int concurrent_policy_time_show(struct seq_file *m, void *v)
 {
 	struct uid_entry *uid_entry;
 	struct task_struct *task, *temp;
 	struct cpufreq_policy *policy;
 	struct cpufreq_policy *last_policy = NULL;
+	u64 buf[num_possible_cpus()];
+	u64 uid, time;
 	unsigned long bkt, flags;
-	int i, cnt = -1, num_possible_cpus = num_possible_cpus();
+	int i, cnt = 0, num_possible_cpus = num_possible_cpus();
 
 	if (!cpufreq_all_freq_init)
 		return 0;
 
 	for_each_possible_cpu(i) {
 		policy = cpufreq_cpu_get(i);
-		if (!policy) {
-			seq_printf(m, " -1");
+		if (!policy)
 			continue;
-		}
-
 		if (policy != last_policy) {
-			if (cnt >= 0)
-				seq_printf(m, "%d ", cnt);
-			cnt = 0;
-			seq_printf(m, "policy%d: ",
-				cpumask_first(policy->related_cpus));
-
+			cnt++;
 			if (last_policy)
 				cpufreq_cpu_put(last_policy);
 			last_policy = policy;
+			buf[cnt] = 0;
 		} else {
 			cpufreq_cpu_put(policy);
 		}
-		++cnt;
+		++buf[cnt];
 	}
 	if (last_policy)
 		cpufreq_cpu_put(last_policy);
 
-	if (cnt >= 0)
-		seq_printf(m, "%d ", cnt);
-
-	seq_putc(m, '\n');
+	buf[0] = (u64) cnt;
+	seq_write(m, buf, (cnt + 1) * sizeof(*buf));
 
 	rt_mutex_lock(&uid_lock);
 
@@ -435,7 +514,8 @@ static int uid_concurrent_policy_time_show(struct seq_file *m, void *v)
 			!uid_entry->dead_concurrent_policy_time)
 			continue;
 
-		seq_printf(m, "%d:", uid_entry->uid);
+		uid = (u64) uid_entry->uid;
+		seq_write(m, &uid, sizeof(uid));
 
 		for (i = 0; i < num_possible_cpus; ++i) {
 			u64 total_concurrent_policy_time = 0;
@@ -446,11 +526,10 @@ static int uid_concurrent_policy_time_show(struct seq_file *m, void *v)
 			if (uid_entry->alive_concurrent_policy_time)
 				total_concurrent_policy_time +=
 					uid_entry->alive_concurrent_policy_time[i];
-			seq_printf(m, " %lu", (unsigned long)
-				cputime_to_clock_t(total_concurrent_policy_time));
+			time = (u64)
+				cputime_to_clock_t(total_concurrent_policy_time);
+			seq_write(m, &time, sizeof(time));
 		}
-
-		seq_putc(m, '\n');
 
 		if (uid_entry->alive_concurrent_policy_time) {
 			kfree(uid_entry->alive_concurrent_policy_time);
@@ -1422,25 +1501,37 @@ static const struct file_operations uid_time_in_state_fops = {
 	.release	= single_release,
 };
 
-static int uid_concurrent_active_time_open(struct inode *inode, struct file *file)
+int time_in_state_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, uid_concurrent_active_time_show, PDE_DATA(inode));
+	return single_open(file, time_in_state_show, PDE_DATA(inode));
 }
 
-static const struct file_operations uid_concurrent_active_time_fops = {
-	.open		= uid_concurrent_active_time_open,
+const struct file_operations time_in_state_fops = {
+	.open		= time_in_state_open,
 	.read		= seq_read,
 	.llseek		= seq_lseek,
 	.release	= single_release,
 };
 
-static int uid_concurrent_policy_time_open(struct inode *inode, struct file *file)
+static int concurrent_active_time_open(struct inode *inode, struct file *file)
 {
-	return single_open(file, uid_concurrent_policy_time_show, PDE_DATA(inode));
+	return single_open(file, concurrent_active_time_show, PDE_DATA(inode));
 }
 
-static const struct file_operations uid_concurrent_policy_time_fops = {
-	.open		= uid_concurrent_policy_time_open,
+static const struct file_operations concurrent_active_time_fops = {
+	.open		= concurrent_active_time_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int concurrent_policy_time_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, concurrent_policy_time_show, PDE_DATA(inode));
+}
+
+static const struct file_operations concurrent_policy_time_fops = {
+	.open		= concurrent_policy_time_open,
 	.read		= seq_read,
 	.llseek		= seq_lseek,
 	.release	= single_release,
@@ -1507,13 +1598,22 @@ static int __init cpufreq_stats_init(void)
 	proc_create_data("uid_time_in_state", 0444, NULL,
 		&uid_time_in_state_fops, NULL);
 
-	proc_create_data("uid_concurrent_active_time", 0444, NULL,
-		&uid_concurrent_active_time_fops, NULL);
-
-	proc_create_data("uid_concurrent_policy_time", 0444, NULL,
-		&uid_concurrent_policy_time_fops, NULL);
-
 	profile_event_register(PROFILE_TASK_EXIT, &process_notifier_block);
+
+	uid_cpupower = proc_mkdir("uid_cpupower", NULL);
+	if (!uid_cpupower) {
+		pr_warn("%s: failed to create uid_cputime proc entry\n",
+			__func__);
+	} else {
+		proc_create_data("time_in_state", 0444, uid_cpupower,
+			&time_in_state_fops, NULL);
+
+		proc_create_data("concurrent_active_time", 0444, uid_cpupower,
+			&concurrent_active_time_fops, NULL);
+
+		proc_create_data("concurrent_policy_time", 0444, uid_cpupower,
+			&concurrent_policy_time_fops, NULL);
+	}
 
 	cpufreq_all_freq_init = true;
 	return 0;